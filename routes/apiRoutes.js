07245ef306cd6826654b245ba92873f4d97b7c00
Kiri <kirilynne@gmail.com>
2021-11-14 16:29:34 -0500

    add content to apiRoutes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,45 @@
+const db = require("../models");
+const router = require("express").Router();
+
+
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+})
+
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+        { _id: req.params.id }, { workout: req.body }
+    )
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", ({ }, res) => {
+    db.Workout.find({})
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+
+module.exports = router;
